b118152794d931fd305a741db918e86fad23c088
malakcharty <malakcharty1@gmail.com>
2025-05-12 20:40:28 +0100

    commit security jdbc

--- src/main/java/com/example/hospital/HospitalApplication.java
+++ src/main/java/com/example/hospital/HospitalApplication.java
@@ -1,13 +1,16 @@
 package com.example.hospital;
 import com.example.hospital.entities.Patient;
 import com.example.hospital.repository.PatientRepository;
+import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -26,6 +29,34 @@ public class HospitalApplication implements CommandLineRunner {
         //patientRepository.save(new Patient(null,"Hanane",new Date(),false,4321));
         //patientRepository.save(new Patient(null,"Imane",new Date(),true,334));
 
+    }
+    @Bean
+    JdbcUserDetailsManager jdbcUserDetailsManager(javax.sql.DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager){
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        return args -> {
+            if (!jdbcUserDetailsManager.userExists("user11")) {
+                jdbcUserDetailsManager.createUser(User.withUsername("user11")
+                        .password(passwordEncoder.encode("1234"))
+                        .roles("USER").build());
+            }
+
+            if (!jdbcUserDetailsManager.userExists("user22")) {
+                jdbcUserDetailsManager.createUser(User.withUsername("user22")
+                        .password(passwordEncoder.encode("1234"))
+                        .roles("USER").build());
+            }
+
+            if (!jdbcUserDetailsManager.userExists("admin2")) {
+                jdbcUserDetailsManager.createUser(User.withUsername("admin2")
+                        .password(passwordEncoder.encode("1234"))
+                        .roles("USER", "ADMIN").build());
+            }
+
+        };
     }
     @Bean
     PasswordEncoder  passwordEncoder(){
